079a25a1aa69b32578095a4040f9eefa3a659e64
Kei Peralta <cnmperalta@gmail.com>
2017-12-20 09:58:28 -0500

    Implement ReverseString.reverse()

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -2,6 +2,10 @@ package quiz;
 
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+        StringBuilder reversedInput = new StringBuilder(input.length());
+        if(input.isEmpty()) return input;
+        for(int  i = input.length()-1; i >=0; i--)
+            reversedInput.append(input.charAt(i));
+        return reversedInput.toString();
     }
 }
